ae82c6968bf3c06b0d9280c05cf3438619012c37
SooHyeon <dame95@naver.com>
2022-07-21 04:16:51 +0900

    add video and image download function

--- crawler.py
+++ crawler.py
@@ -12,6 +12,32 @@ import pyperclip
 
 class Crawler:
 
+    category_title = 'style-scope ytd-watch-metadata' # 강좌명
+    category_sub_title = "" #부-강좌명
+    category_intro = "" #강의 소개
+    category_thumbnail = 'style-scope yt-img-shadow' #썸네일
+    category_difficulty = "" #난이도
+    category_tag = "" # 태그
+    category_for_recommend = "" # ??? 강의 소개
+    category_paid = "" # 가격?
+    category_teacher = 'yt-simple-endpoint style-scope yt-formatted-string' # 강의자
+
+    category_url = 'style-scope ytd-playlist-panel-renderer' #해당 강의 카테고리 url
+
+    category_review = 'style-scope ytd-comment-renderer' # 수강평
+    category_body = '' # 강의 소개
+    category_pay_div = False # ??? 강의 난이도 boolean
+    category_curriculum_summary = 'style-scope ytd-playlist-panel-video-renderer' # 커리큘럼
+
+    content_url = 'style-scope ytd-playlist-panel-renderer' #해당 카테고리내의 개별 강의 url
+
+    content_title = 'style-scope ytd-watch-metadata' # 컨텐츠명
+    content_order = '' # 컨텐츠 순서(컨텐츠명에서 떼오면 될듯)
+    content_type = '' # ??? 컨텐츠
+    content_data_url = '' # 강의 영상 링크
+    content_data_body = 'style-scope yt-formatted-string' # ??? 내용
+    content_data_description = "yt-simple-endpoint style-scope yt-formatted-string" # ??? 학습 목표 및 부가 정보
+
     def __init__(self):
         service = Service(executable_path=ChromeDriverManager().install())
 
@@ -22,7 +48,7 @@ class Crawler:
         # chrome_options.add_argument('--log-level=1')
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
-        self.driver.implicitly_wait(4)
+        self.driver.implicitly_wait(5)
         self.driver.maximize_window()
         print('-----------------------driver init-----------------------')
     
@@ -30,70 +56,81 @@ class Crawler:
         self.driver.close()
         print("-------------------driver closed-------------------")
 
-    def setCrawler(self,LectureName):
+    def setCrawler(self,url):
         print('-------------------------set crawler-------------------------')
-        self.driver.get(LectureName)
+        self.driver.get(url)
         time.sleep(2)
 
     ##################### category url ###################################
 
     def get_category_title(self):
-        return self.driver.find_element(By.CLASS_NAME, 'course_title').get_attribute('innerText')
+        print(Crawler.category_title)
+        return self.driver.find_element(By.CLASS_NAME, Crawler.category_title).text
+
+    def get_category_sub_title(self):
+        return self.driver.find_element(By.CLASS_NAME, Crawler.category_sub_title).text
 
     def get_category_intro(self):
-        return self.driver.find_element(By.CLASS_NAME, 'course_description').get_attribute('innerText')
+        return self.driver.find_element(By.CLASS_NAME, Crawler.category_intro).get_attribute('innerText')
 
     def get_category_thumbnail(self):
-        return self.driver.find_element(By.TAG_NAME, 'img').get_attribute('src')
+        return self.driver.find_element(By.TAG_NAME, Crawler.category_thumbnail).get_attribute('src')
 
     def get_category_difficulty(self):
-        return self.driver.find_element(By.CLASS_NAME, 'course_level').get_attribute('innerText')
+        return self.driver.find_element(By.CLASS_NAME, Crawler.category_difficulty).get_attribute('innerText')
 
     def get_category_tag(self):
-        return self.driver.find_element(By.CLASS_NAME, '').get_attribute('')
+        return self.driver.find_element(By.CLASS_NAME, Crawler.category_tag).get_attribute('')
 
     def get_category_for_recommend(self):
-        return self.driver.find_element(By.CLASS_NAME, '').get_attribute('')
+        return self.driver.find_element(By.CLASS_NAME, Crawler.category_for_recommend).get_attribute('')
 
     def get_category_paid(self):
-        return self.driver.find_element(By.CLASS_NAME, '').get_attribute('')
+        return self.driver.find_element(By.CLASS_NAME, Crawler.category_paid).get_attribute('')
 
     def get_category_teacher(self):
-        return self.driver.find_element(By.CLASS_NAME, 'instructor').get_attribute('innerText')
+        return self.driver.find_element(By.CLASS_NAME, Crawler.category_teacher).get_attribute('innerText')
 
-    ####################### lecture url #####################################
+    def get_category_url(self):
+        return self.driver.find_element(By.CLASS_NAME, Crawler.category_url).get_attribute('href')
 
-    def get_lecture_url(self):
-        return self.driver.find_element(By.CLASS_NAME, 'course_card_front').get_attribute('href')
+    def get_category_review(self):
+        return self.driver.find_element(By.CLASS_NAME, Crawler.category_review).get_attribute('innerText')
 
-    def get_lecture_review(self):
-        return self.driver.find_element(By.CLASS_NAME, 'review-list__content').get_attribute('innerText')
-
-    def get_lecture_body(self):
-        return self.driver.find_element(By.CLASS_NAME, 'cd-body').get_attribute('innerText')
+    def get_category_body(self):
+        return self.driver.find_element(By.CLASS_NAME, Crawler.category_body).get_attribute('innerText')
     
-    def get_lecture_pay_div(self):
-        return self.driver.find_element(By.CLASS_NAME, 'course_card_front').get_attribute('innerText')
+    def get_category_pay_div(self):
+        return self.driver.find_element(By.CLASS_NAME, Crawler.category_pay_div).get_attribute('innerText')
 
-    def get_lecture_curriculum_summary(self):
-        return self.driver.find_element(By.CLASS_NAME, 'cd-curriculum').get_attribute('innerText')
+    def get_category_curriculum_summary(self):
+        arr = []
+        for temp in self.driver.find_elements(By.CLASS_NAME, Crawler.category_curriculum_summary):
+            arr.append(temp.text)
+        return arr
 
     ######################## contents url ###################################
 
+    def get_content_url(self):
+        arr = []
+        for temp in self.driver.find_elements(By.CLASS_NAME, Crawler.content_url):
+            arr.append(temp.get_attribute('href'))
+        return arr
+
     def get_content_title(self):
-        return self.driver.find_element(By.CLASS_NAME, 'css-1pqj6dl').get_attribute('innerText')
+        return self.driver.find_element(By.CLASS_NAME, Crawler.content_title).get_attribute('innerText')
 
     def get_content_order(self):
-        return self.driver.find_element(By.CLASS_NAME, '').get_attribute('')
+        return self.driver.find_element(By.CLASS_NAME, Crawler.content_order).get_attribute('')
 
     def get_content_type(self):
-        return self.driver.find_element(By.CLASS_NAME, 'css-1pqj6dl').get_attribute('innerText')
+        return self.driver.find_element(By.CLASS_NAME, Crawler.content_type).get_attribute('innerText')
 
     def get_content_data_url(self):
-        return self.driver.find_element(By.CLASS_NAME, 'css-w1atjl shaka-video').get_attribute('currentSrc')
+        return self.driver.find_element(By.CLASS_NAME, Crawler.content_data_url).get_attribute('href')
 
     def get_content_data_body(self):
-        return self.driver.find_element(By.CLASS_NAME, '').get_attribute('')
+        return self.driver.find_element(By.CLASS_NAME, Crawler.content_data_body).get_attribute('')
 
     def get_content_data_description(self):
-        return self.driver.find_element(By.CLASS_NAME, '').get_attribute('')
+        return self.driver.find_element(By.CLASS_NAME, Crawler.content_data_description).get_attribute('')
