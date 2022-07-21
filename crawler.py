8e1f080859c903c00e88140b7d8b86b0bb9188f0
SooHyeon <dame95@naver.com>
2022-07-21 15:29:51 +0900

    add function (store json files, download video)

--- crawler.py
+++ crawler.py
@@ -1,136 +1,136 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
 
-#chrome driver auto update
-from webdriver_manager.chrome import ChromeDriverManager
-import time
-import pyautogui
-import pyperclip
+# #chrome driver auto update
+# from webdriver_manager.chrome import ChromeDriverManager
+# import time
+# import pyautogui
+# import pyperclip
 
-class Crawler:
+# class Crawler:
 
-    category_title = 'style-scope ytd-watch-metadata' # 강좌명
-    category_sub_title = "" #부-강좌명
-    category_intro = "" #강의 소개
-    category_thumbnail = 'style-scope yt-img-shadow' #썸네일
-    category_difficulty = "" #난이도
-    category_tag = "" # 태그
-    category_for_recommend = "" # ??? 강의 소개
-    category_paid = "" # 가격?
-    category_teacher = 'yt-simple-endpoint style-scope yt-formatted-string' # 강의자
+#     category_title = 'style-scope ytd-watch-metadata' # 강좌명
+#     category_sub_title = "" #부-강좌명
+#     category_intro = "" #강의 소개
+#     category_thumbnail = 'style-scope yt-img-shadow' #썸네일
+#     category_difficulty = "" #난이도
+#     category_tag = "" # 태그
+#     category_for_recommend = "" # ??? 강의 소개
+#     category_paid = "" # 가격?
+#     category_teacher = 'yt-simple-endpoint style-scope yt-formatted-string' # 강의자
 
-    category_url = 'style-scope ytd-playlist-panel-renderer' #해당 강의 카테고리 url
+#     category_url = 'style-scope ytd-playlist-panel-renderer' #해당 강의 카테고리 url
 
-    category_review = 'style-scope ytd-comment-renderer' # 수강평
-    category_body = '' # 강의 소개
-    category_pay_div = False # ??? 강의 난이도 boolean
-    category_curriculum_summary = 'style-scope ytd-playlist-panel-video-renderer' # 커리큘럼
+#     category_review = 'style-scope ytd-comment-renderer' # 수강평
+#     category_body = '' # 강의 소개
+#     category_pay_div = False # ??? 강의 난이도 boolean
+#     category_curriculum_summary = 'style-scope ytd-playlist-panel-video-renderer' # 커리큘럼
 
-    content_url = 'style-scope ytd-playlist-panel-renderer' #해당 카테고리내의 개별 강의 url
+#     content_url = 'style-scope ytd-playlist-panel-renderer' #해당 카테고리내의 개별 강의 url
 
-    content_title = 'style-scope ytd-watch-metadata' # 컨텐츠명
-    content_order = '' # 컨텐츠 순서(컨텐츠명에서 떼오면 될듯)
-    content_type = '' # ??? 컨텐츠
-    content_data_url = '' # 강의 영상 링크
-    content_data_body = 'style-scope yt-formatted-string' # ??? 내용
-    content_data_description = "yt-simple-endpoint style-scope yt-formatted-string" # ??? 학습 목표 및 부가 정보
+#     content_title = 'style-scope ytd-watch-metadata' # 컨텐츠명
+#     content_order = '' # 컨텐츠 순서(컨텐츠명에서 떼오면 될듯)
+#     content_type = '' # ??? 컨텐츠
+#     content_data_url = '' # 강의 영상 링크
+#     content_data_body = 'style-scope yt-formatted-string' # ??? 내용
+#     content_data_description = "yt-simple-endpoint style-scope yt-formatted-string" # ??? 학습 목표 및 부가 정보
 
-    def __init__(self):
-        service = Service(executable_path=ChromeDriverManager().install())
+#     def __init__(self):
+#         service = Service(executable_path=ChromeDriverManager().install())
 
-        chrome_options = Options()
-        chrome_options.add_experimental_option("detach",True)
-        chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        # chrome_options.add_argument('--headless')
-        # chrome_options.add_argument('--log-level=1')
-        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+#         chrome_options = Options()
+#         chrome_options.add_experimental_option("detach",True)
+#         chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+#         # chrome_options.add_argument('--headless')
+#         # chrome_options.add_argument('--log-level=1')
+#         self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
-        self.driver.implicitly_wait(5)
-        self.driver.maximize_window()
-        print('-----------------------driver init-----------------------')
+#         self.driver.implicitly_wait(5)
+#         self.driver.maximize_window()
+#         print('-----------------------driver init-----------------------')
     
-    def __del__(self):
-        self.driver.close()
-        print("-------------------driver closed-------------------")
+#     def __del__(self):
+#         self.driver.close()
+#         print("-------------------driver closed-------------------")
 
-    def setCrawler(self,url):
-        print('-------------------------set crawler-------------------------')
-        self.driver.get(url)
-        time.sleep(2)
+#     def setCrawler(self,url):
+#         print('-------------------------set crawler-------------------------')
+#         self.driver.get(url)
+#         time.sleep(2)
 
-    ##################### category url ###################################
+#     ##################### category url ###################################
 
-    def get_category_title(self):
-        print(Crawler.category_title)
-        return self.driver.find_element(By.CLASS_NAME, Crawler.category_title).text
+#     def get_category_title(self):
+#         print(Crawler.category_title)
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.category_title).text
 
-    def get_category_sub_title(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.category_sub_title).text
+#     def get_category_sub_title(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.category_sub_title).text
 
-    def get_category_intro(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.category_intro).get_attribute('innerText')
+#     def get_category_intro(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.category_intro).get_attribute('innerText')
 
-    def get_category_thumbnail(self):
-        return self.driver.find_element(By.TAG_NAME, Crawler.category_thumbnail).get_attribute('src')
+#     def get_category_thumbnail(self):
+#         return self.driver.find_element(By.TAG_NAME, Crawler.category_thumbnail).get_attribute('src')
 
-    def get_category_difficulty(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.category_difficulty).get_attribute('innerText')
+#     def get_category_difficulty(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.category_difficulty).get_attribute('innerText')
 
-    def get_category_tag(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.category_tag).get_attribute('')
+#     def get_category_tag(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.category_tag).get_attribute('')
 
-    def get_category_for_recommend(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.category_for_recommend).get_attribute('')
+#     def get_category_for_recommend(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.category_for_recommend).get_attribute('')
 
-    def get_category_paid(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.category_paid).get_attribute('')
+#     def get_category_paid(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.category_paid).get_attribute('')
 
-    def get_category_teacher(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.category_teacher).get_attribute('innerText')
+#     def get_category_teacher(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.category_teacher).get_attribute('innerText')
 
-    def get_category_url(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.category_url).get_attribute('href')
+#     def get_category_url(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.category_url).get_attribute('href')
 
-    def get_category_review(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.category_review).get_attribute('innerText')
+#     def get_category_review(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.category_review).get_attribute('innerText')
 
-    def get_category_body(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.category_body).get_attribute('innerText')
+#     def get_category_body(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.category_body).get_attribute('innerText')
     
-    def get_category_pay_div(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.category_pay_div).get_attribute('innerText')
+#     def get_category_pay_div(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.category_pay_div).get_attribute('innerText')
 
-    def get_category_curriculum_summary(self):
-        arr = []
-        for temp in self.driver.find_elements(By.CLASS_NAME, Crawler.category_curriculum_summary):
-            arr.append(temp.text)
-        return arr
+#     def get_category_curriculum_summary(self):
+#         arr = []
+#         for temp in self.driver.find_elements(By.CLASS_NAME, Crawler.category_curriculum_summary):
+#             arr.append(temp.text)
+#         return arr
 
-    ######################## contents url ###################################
+#     ######################## contents url ###################################
 
-    def get_content_url(self):
-        arr = []
-        for temp in self.driver.find_elements(By.CLASS_NAME, Crawler.content_url):
-            arr.append(temp.get_attribute('href'))
-        return arr
+#     def get_content_url(self):
+#         arr = []
+#         for temp in self.driver.find_elements(By.CLASS_NAME, Crawler.content_url):
+#             arr.append(temp.get_attribute('href'))
+#         return arr
 
-    def get_content_title(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.content_title).get_attribute('innerText')
+#     def get_content_title(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.content_title).get_attribute('innerText')
 
-    def get_content_order(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.content_order).get_attribute('')
+#     def get_content_order(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.content_order).get_attribute('')
 
-    def get_content_type(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.content_type).get_attribute('innerText')
+#     def get_content_type(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.content_type).get_attribute('innerText')
 
-    def get_content_data_url(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.content_data_url).get_attribute('href')
+#     def get_content_data_url(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.content_data_url).get_attribute('href')
 
-    def get_content_data_body(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.content_data_body).get_attribute('')
+#     def get_content_data_body(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.content_data_body).get_attribute('')
 
-    def get_content_data_description(self):
-        return self.driver.find_element(By.CLASS_NAME, Crawler.content_data_description).get_attribute('')
+#     def get_content_data_description(self):
+#         return self.driver.find_element(By.CLASS_NAME, Crawler.content_data_description).get_attribute('')
--- parser.py
+++ parser.py
@@ -1,16 +1,15 @@
-from crawler import Crawler as sc 
-from pytube import YouTube, exceptions, metadata, Playlist
+# from crawler import Crawler as sc 
+from pytube import YouTube, exceptions, Playlist
 import json
 import os
 
-
 programming_sub_category = {
-    "BasicProgramming" :"https://www.youtube.com/watch?v=-3DHpwy498o&list=PLuHgQVnccGMDtnr4nTSFfmocHL5FeH1xR&index=1"
-    # ,"WebProgramming" : "https://www.youtube.com/watch?v=tZooW6PritE&list=PLuHgQVnccGMDZP7FJ_ZsUrdCGH68ppvPb"
-    # ,"DevOps":"https://www.youtube.com/watch?v=Ps8HDIAyPD0&list=PLuHgQVnccGMDeMJsGq2O-55Ymtx0IdKWf"
-    # ,"AI":"https://www.youtube.com/watch?v=LPqmPfhnR1o&list=PLuHgQVnccGMDy5oF7G5WYxLF3NCYhB9H9"
-    # ,"DB":"https://www.youtube.com/watch?v=h_XDmyz--0w&list=PLuHgQVnccGMCgrP_9HL3dAcvdt8qOZxjW"
-    # ,"Algorithm":"https://www.youtube.com/watch?v=bj2F0hTiTtw&list=PLuHgQVnccGMDsWOOn_P0EmAWB8DArS3Fk"
+    "BasicProgramming" : "https://www.youtube.com/watch?v=-3DHpwy498o&list=PLuHgQVnccGMDtnr4nTSFfmocHL5FeH1xR&index=1"
+    , "WebProgramming" : "https://www.youtube.com/watch?v=tZooW6PritE&list=PLuHgQVnccGMDZP7FJ_ZsUrdCGH68ppvPb"
+    , "DevOps": "https://www.youtube.com/watch?v=Ps8HDIAyPD0&list=PLuHgQVnccGMDeMJsGq2O-55Ymtx0IdKWf"
+    , "AI": "https://www.youtube.com/watch?v=LPqmPfhnR1o&list=PLuHgQVnccGMDy5oF7G5WYxLF3NCYhB9H9"
+    , "DB": "https://www.youtube.com/watch?v=h_XDmyz--0w&list=PLuHgQVnccGMCgrP_9HL3dAcvdt8qOZxjW"
+    , "Algorithm": "https://www.youtube.com/watch?v=bj2F0hTiTtw&list=PLuHgQVnccGMDsWOOn_P0EmAWB8DArS3Fk"
     }
 
 business_sub_category = {
@@ -27,135 +26,114 @@ categories = {
 #    ,"Design" : design_sub_category
 }
 
-category_title = "" # 강좌명
+category_title = "" # 강좌명 required
 category_sub_title = "" #부-강좌명
-category_intro = "" #강의 소개
-category_thumbnail = [] #썸네일
+category_intro = "" #강의 소개 
+category_thumbnail = [] #썸네일 
 category_difficulty = '중' #난이도
 category_tag = [] # 태그
-category_for_recommend = [] # ??? 강의 소개
-category_paid = 0 # 가격?
+category_for_recommend = [] # 수강 추천 대상
+category_paid = 0 # 가격
 category_review = [] # 수강평
 category_body = "" # 강의 소개
-category_pay_div = True # ??? 강의 난이도 boolean
-category_teacher = "" # 강의자
+category_pay_div = True # 유무료 여부
+category_teacher = "" # 강의자 required
 category_curriculum_summary = [] # 커리큘럼
-content_title = '' # 컨텐츠명
-content_order = 0 # 컨텐츠 순서(컨텐츠명에서 떼오면 될듯)
-content_type = 'video' # ??? 컨텐츠
+content_title = '' # 컨텐츠명 required
+content_order = 0 # 컨텐츠 순서
+content_type = 'video' # 컨텐츠 타입(영상, 문서, ...) required
 content_data_url = '' # 강의 영상 링크
-content_data_body = '' # ??? 내용
-content_data_description = '' # ??? 학습 목표 및 부가 정보
+content_data_body = '' # 강의 설명
+content_data_description = '' # 강의 상세 설명
 
-video_down_dir = './video'
-image_down_dir = './image/'
-
-if not os.path.exists('./image/'):
-    os.mkdir('./image/')
-if not os.path.exists('./video/'):
-    os.mkdir('./video/')
+# image_down_dir = './image/'
 
 
-crawler = sc()
+# if not os.path.exists('./image/'):
+#     os.mkdir('./image/')
 
+
+################################## 카테고리 ###################################
 for category_name, category  in categories.items():
+    ################################## 부-카테고리 #####################################
     for sub_category_name, sub_category_url in category.items():
         print(sub_category_url) 
 
         ############################# 이미지 다운로드 #############################
-        category_thumbnail = YouTube(sub_category_url).thumbnail_url
-        filename = YouTube(sub_category_url).title.replace('\\','').replace('/','').replace(':','').replace('*','').replace('?','').replace('"','').replace('<','').replace('>','').replace('|','')
-        try:
-            os.system('curl '+ category_thumbnail + ' > ' + image_down_dir + ''.join(filename.split()) + '.jpg')
-        except OSError:
-            print(exceptions)
-        
-        crawler.setCrawler(sub_category_url)
-        print("강좌명")
-        temp = crawler.get_category_title().split('\n')
-        t = temp[0].split(':')
-        print(t)
-        category_title = t[0]
-        category_sub_title = t[-1]
+        # category_thumbnail = YouTube(sub_category_url).thumbnail_url
+        # filename = YouTube(sub_category_url).title.replace('\\','').replace('/','').replace(':','').replace('*','').replace('?','').replace('"','').replace('<','').replace('>','').replace('|','')
+        # try:
+        #     os.system('curl '+ category_thumbnail + ' > ' + image_down_dir + ''.join(filename.split()) + '.jpg')
+        # except OSError:
+        #     print(exceptions)
 
-        ############################# LectureSchema 저장 ################################
-        # print("난이도")
-        # print(crawler.get_category_difficulty())
-        # print("강의 소개")
-        # print(crawler.get_category_intro())
-        print("썸네일")
-        print(category_thumbnail)
-        # print(crawler.get_category_thumbnail())
-        # print("강의자")
-        # print(crawler.get_category_teacher())
-        # crawler.setCrawler(crawler.get_category_url())
-        # print("수강평")
-        # print(crawler.get_category_review())
-        print("커리큘럼")
-        category_curriculum_summary.append(crawler.get_category_curriculum_summary())
-        # print("영상 링크")
-        # print(crawler.get_content_data_url())
-        
-        # print(driver)
-    
-        print(category_title)
-        print(category_sub_title)
-        print(category_curriculum_summary)
-
-        ##################### 재생 목록에서 받아올 수 있는 부분 #######################
-        # LectureSchema = {
-        #     "category" : category_name , #{ type: String, required: true }, // 카테고리
-        #     "sub_category" : sub_category_name , #{ type: String, default:"" }, //카테고리 -부
-        #     "title" : , #{ type: String, required: true }, // 강좌명
-        #     "sub_title" : , # { type: String, default:"" }, // 부 제목
-        #     "intro" : , #{ type: String, default:"" }, // 강의 소개
-        #     "thumbnail" : , #{ type: Array, default:[] }, // 강의 소개
-        #     "difficulty" : , #{ type: String, default:"중" }, // 강의 소개
-        #     "tag" : , #{ type: Array, default:[] }, // 강의 소개
-        #     "for_recommend" : , #{ type: Array, default:[] }, // 강의 대상
-        #     "paid" : , #{ type: Number, default:0 }, // 가격
-        #     "review" : , #[{ type: ObjectId , ref: "review" }], // 신청 강좌 정보(댓글)
-        #     "body" : , #{ type: String, default:"" }, // 강의 요약 정보
-        #     "pay_div" : , #{ type: Boolean, default:"중" }, // 강의 유무료 여부
-        #     "teacher" : , #[{ type: ObjectId , required: true, ref: "teacher" }], // 강의자
-        #     "curriculum_summary" :  #{ type: Array, default:[] } // 신청 강좌 주차 요약 정보
-        # }
-        # contents_url = crawler.get_content_url()
-        # print(contents_url)
         pl = Playlist(sub_category_url)
         
+        category_title = pl.title
+        category_teacher = pl.owner
+        
+        if not os.path.exists('./jsonfile/' + sub_category_name):
+            os.mkdir('./jsonfile/' + sub_category_name)
+
+        video_down_dir = './video/' + sub_category_name+'/'
+
+        if not os.path.exists('./video/'+sub_category_name+'/'):
+            os.mkdir('./video/' + sub_category_name+'/')
+        ######################################## 개별 컨텐츠 작업 ############################################
         for order, content in enumerate(pl.video_urls):
             print(content)
-            crawler.setCrawler(content)
 
             ############################## 영상 다운로드 ##############################
             try:
                 yt = YouTube(content)
-                title = yt.title
             except exceptions.VideoUnavailable:
-                print(f'Video {title} is unavaialable, skipping.')
+                print(f'Video {content} is unavaialable, skipping.')
             else:
-                print(f'Downloading video: {title}')
-                print(yt.streams.filter(resolution='360p',file_extension='mp4',progressive=True))
+                print(f'Downloading video: {content}')
+                yt.streams.filter(resolution='360p',file_extension='mp4',progressive=True)
                 yt.streams.get_by_itag(18).download(video_down_dir)
 
-            
+            title = yt.title.replace('\\',' ').replace('/',' ').replace(':','-').replace('*',' ').replace('?',' ').replace('"',"'").replace('<','[').replace('>',']').replace('|',' ')
 
-            ##################### 개별 영상에서 받아올 수 있는 부분 #######################
-            # ContentSchema = {
-            #     "title": title , #{ type: String, required: true },// 컨텐츠명
-            #     "order" : order , #{ type: Number, default: 0 },// 콘텐츠 순서
-            #     "type" : content_type , #{ type: String, required: true }, // 콘텐츠 타입
-            #     "data" : {
-            #         "url" : content, #{ type: String, default:"" }, // 영상 및 자료 링크
-            #         "body" : content_data_body, #{ type: String, default:""  }, // 내용
-            #         "description" : content_data_description #{ type: String, default:""  }, // 학습 목표 및 부가 정보
-            #     }
-            # }
+            # print('video id : ' + yt.video_id)
+            describe = yt.description.split('\n')
+            content_data_body = describe[0]
+            content_data_description = ' '.join(describe[1:])
+            category_curriculum_summary.append(title)
+            #################### 개별 영상에서 받아올 수 있는 부분 #######################
+            ContentSchema = {
+                "title": title , #{ type: String, required: true },// 컨텐츠명
+                "order" : order , #{ type: Number, default: 0 },// 콘텐츠 순서
+                "type" : content_type , #{ type: String, required: true }, // 콘텐츠 타입
+                "data" : {
+                    "url" : content, #{ type: String, default:"" }, // 영상 및 자료 링크
+                    "body" : content_data_body, #{ type: String, default:""  }, // 내용
+                    "description" : content_data_description #{ type: String, default:""  }, // 학습 목표 및 부가 정보
+                }
+            }
+            
             ########################## content -> json 파일로 저장 #########################
-            #with open('./' + filename +'.json', 'w') as f:
-            #json.dump(ContentSchema, f, indent=2)
+            with open('./jsonfile/' + sub_category_name + '/' + title +'.json', 'w', encoding='UTF-8') as f:
+                json.dump(ContentSchema, f, indent=2, ensure_ascii=False)
+        ##################### 재생 목록에서 받아올 수 있는 부분 #######################
+        LectureSchema = {
+            "category" : category_name , #{ type: String, required: true }, // 카테고리
+            "sub_category" : sub_category_name , #{ type: String, default:"" }, //카테고리 -부
+            "title" :  category_title, #{ type: String, required: true }, // 강좌명
+            "sub_title" : category_sub_title, # { type: String, default:"" }, // 부 제목
+            "intro" : category_intro, #{ type: String, default:"" }, // 강의 소개
+            "thumbnail" : category_thumbnail, #{ type: Array, default:[] }, // 강의 소개
+            "difficulty" : category_difficulty, #{ type: String, default:"중" }, // 강의 소개
+            "tag" : category_tag, #{ type: Array, default:[] }, // 강의 소개
+            "for_recommend" : category_for_recommend, #{ type: Array, default:[] }, // 강의 대상
+            "paid" : category_paid, #{ type: Number, default:0 }, // 가격
+            "review" : category_review, #[{ type: ObjectId , ref: "review" }], // 신청 강좌 정보(댓글)
+            "body" : category_body, #{ type: String, default:"" }, // 강의 요약 정보
+            "pay_div" : category_pay_div, #{ type: Boolean, default:"중" }, // 강의 유무료 여부
+            "teacher" : category_teacher, #[{ type: ObjectId , required: true, ref: "teacher" }], // 강의자
+            "curriculum_summary" :  category_curriculum_summary#{ type: Array, default:[] } // 신청 강좌 주차 요약 정보
+        }
         ############################## category -> json 파일로 저장 ##########################
-        #with open('./' + sub_category_name +'.json', 'w') as f:
-        #   json.dump(LectureSchema, f, indent=2)
+        with open('./jsonfile/' + sub_category_name + '/' + sub_category_name +'.json', 'w', encoding='UTF-8') as f:
+          json.dump(LectureSchema, f, indent=2, ensure_ascii=False)
         
